d6d85765cb3cbfad0216e096ff6b9e9aa5e94377
jackwallhack <isaiahjcox10@gmail.com>
2024-11-09 00:31:36 +1000

    Update search.js

--- pages/search.js
+++ pages/search.js
@@ -5,9 +5,8 @@ export default async function handler(req, res) {
   const { username } = req.query;
   
   try {
-    // Fetch player information from Roblox API
     const response = await axios.get(`https://users.roblox.com/v1/users/search?keyword=${username}`);
-    const player = response.data.data[0]; // Assuming it returns a list of players
+    const player = response.data.data[0];
 
     if (!player) {
       return res.status(404).json({ error: 'Player not found' });
