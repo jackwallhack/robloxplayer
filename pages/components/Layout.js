7e9b6b2fb532afac71649af6ce2018264ad2af64
jackwallhack <isaiahjcox10@gmail.com>
2024-11-09 00:29:55 +1000

    Create Layout.js

---
+++ pages/components/Layout.js
@@ -0,0 +1,4 @@
+// components/Layout.js
+export default function Layout({ children }) {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-primary to-secondary flex items-center justify-center text
